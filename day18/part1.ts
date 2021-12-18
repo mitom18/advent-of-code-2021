c85ff48044581cd136fba2415feffdd1cb093f6b
mitom18 <michal.toman99@email.cz>
2021-12-18 22:59:26 +0100

    Day 18 solution

---
+++ day18/part1.ts
@@ -0,0 +1,104 @@
+import { createReadStream } from "fs";
+import { createInterface } from "readline";
+
+const addSnailNumbers = (a: string[], b: string[]) => {
+  return ["[", ...a, ...b, "]"];
+};
+
+const explode = (snailNumber: string[]) => {
+  let opened = 0;
+
+  for (let i = 0; i < snailNumber.length; i++) {
+    const currentChar = snailNumber[i];
+
+    if (currentChar === "[") {
+      opened++;
+    }
+
+    if (currentChar === "]") {
+      opened--;
+    }
+
+    if (opened === 5) {
+      let buf = snailNumber.splice(i, 4);
+      let up = i - 1;
+      let down = i;
+
+      while (down--) {
+        const parsedNumber = parseInt(snailNumber[down]);
+
+        if (!isNaN(parsedNumber)) {
+          snailNumber[down] = (parsedNumber + parseInt(buf[1])).toString();
+          break;
+        }
+      }
+
+      while (up++ < snailNumber.length) {
+        const parsedNumber = parseInt(snailNumber[up]);
+
+        if (!isNaN(parsedNumber)) {
+          snailNumber[up] = (parsedNumber + parseInt(buf[2])).toString();
+          break;
+        }
+      }
+
+      snailNumber.splice(i, 0, "0");
+      explode(snailNumber);
+      return true;
+    }
+  }
+  return false;
+};
+
+const split = (snailNumber: string[]) => {
+  for (let i = 0; i < snailNumber.length; i++) {
+    const currentChar = snailNumber[i];
+    const parsedNumber = parseInt(currentChar);
+    if (!isNaN(parsedNumber) && parsedNumber > 9) {
+      let left = Math.floor(parsedNumber / 2).toString();
+      let right = Math.ceil(parsedNumber / 2).toString();
+      let newNumber = ["[", left, right, "]"];
+      snailNumber.splice(i, 1, ...newNumber);
+      return true;
+    }
+  }
+  return false;
+};
+
+const magnitude = (snailNumber: string[]) => {
+  for (let i = 0; i < snailNumber.length; i++) {
+    const currentChar = snailNumber[i];
+    if (currentChar == "]") {
+      let buf = snailNumber.splice(i - 3, 4).map((c) => parseInt(c));
+      let sum = buf[1] * 3 + buf[2] * 2;
+      snailNumber.splice(i - 3, 0, sum.toString());
+      i -= 3;
+    }
+  }
+  return snailNumber;
+};
+
+async function main() {
+  const fileStream = createReadStream("day18/data.in");
+
+  const rl = createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
+  });
+
+  const numbers = [] as string[][];
+
+  for await (const line of rl) {
+    numbers.push(line.split(",").flatMap((c) => c.split("")));
+  }
+
+  let snailSum = numbers[0];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    snailSum = addSnailNumbers(snailSum, numbers[i + 1]);
+    while (explode(snailSum) || split(snailSum));
+  }
+
+  console.log(parseInt(magnitude(snailSum)[0]));
+}
+
+main();
